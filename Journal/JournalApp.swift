125aa25748dc8c7872a2430d3aef0ff35bdb8cdf
Tim Yoon <stimyoon@gmail.com>
2022-02-14 21:39:04 -0500

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,20 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Tim Yoon on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
