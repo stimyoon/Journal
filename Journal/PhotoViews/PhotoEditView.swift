18290f724e94cf17fda8012dbd837976b650cca8
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-02-21 23:09:55 -0500

    Image picking

--- Journal/PhotoViews/PhotoEditView.swift
+++ Journal/PhotoViews/PhotoEditView.swift
@@ -6,23 +6,37 @@
 //
 
 import SwiftUI
+import AVFAudio
 class PhotoEditViewModel: ObservableObject {
     @Published var title = ""
     @Published var uiImage : UIImage?
 }
 struct PhotoEditView: View {
+    @State var photo = Photo()
+    @State var isShowingSheet = false
     
     var body: some View {
         VStack{
-            Text("Hello")
+            TextField("title", text: $photo.title)
+            if let uiImage = photo.uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+            } else {
+                Image(systemName: "photo")
+            }
+        }
+        .sheet(isPresented: $isShowingSheet, onDismiss: nil) {
+            ImagePicker(uiImage: $photo.uiImage)
         }
         .navigationTitle("Choose Photo")
         .toolbar {
             ToolbarItem{
                 Button {
-                    
+                    isShowingSheet = true
                 } label: {
-                    Text("Add Photo")
+                    Text("Image Picker")
                 }
 
             }
